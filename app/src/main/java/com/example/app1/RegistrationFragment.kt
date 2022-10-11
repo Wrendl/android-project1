769b9aa2a19e1afcfe832babbed16bfa0b672f96
Wrendl-2 <60422821+Wrendl@users.noreply.github.com>
2022-10-11 15:21:14 +0600

    v2.1

--- app/src/main/java/com/example/app1/RegistrationFragment.kt
+++ app/src/main/java/com/example/app1/RegistrationFragment.kt
@@ -2,6 +2,7 @@ package com.example.app1
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -13,6 +14,19 @@ class RegistrationFragment: Fragment(R.layout.register_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        button_register.setOnClickListener {
+
+            if (edit_text_username.text.trim().isNotEmpty() ||
+                edit_text_mail.text.trim().isNotEmpty() ||
+                edit_text_password.text.trim().isNotEmpty() ||
+                edit_text_password_confirm.text.trim().isNotEmpty()) {
+
+            }else {
+                Toast.makeText(activity, "Input required", Toast.LENGTH_LONG).show()
+            }
+
+        }
 
         button_to_login.setOnClickListener {
             val action = RegistrationFragmentDirections.actionRegistrationFragmentToLoginFragment()
